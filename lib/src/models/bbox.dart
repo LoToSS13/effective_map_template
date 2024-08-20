ff6a1502bb182ec5624c9ff32b45ebb48a7c8366
Алексей <azubankov@gemsdev.com>
2024-08-20 09:56:59 +0600

    [+] Расширены методы у BBox

--- lib/src/models/bbox.dart
+++ lib/src/models/bbox.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as math;
+import 'package:effective_map/src/utils/geometry_converter.dart';
 import 'package:meta/meta.dart';
 
 import 'package:effective_map/src/models/latlng.dart';
@@ -8,6 +10,33 @@ class BBox {
   final LatLng lowerCorner;
 
   const BBox({required this.upperCorner, required this.lowerCorner});
+
+  LatLng get center {
+    /* https://stackoverflow.com/a/4656937
+       http://www.movable-type.co.uk/scripts/latlong.html
+
+       coord 1: lowerCorner
+       coord 2: upperCorner
+
+       phi: lat
+       lambda: lng
+    */
+
+    final phi1 = lowerCorner.latitudeInRad;
+    final lambda1 = lowerCorner.longitudeInRad;
+    final phi2 = upperCorner.latitudeInRad;
+
+    final dLambda = degrees2Radians * (upperCorner.longitude - lowerCorner.longitude); // delta lambda = lambda2-lambda1
+
+    final bx = math.cos(phi2) * math.cos(dLambda);
+    final by = math.cos(phi2) * math.sin(dLambda);
+    final phi3 =
+        math.atan2(math.sin(phi1) + math.sin(phi2), math.sqrt((math.cos(phi1) + bx) * (math.cos(phi1) + bx) + by * by));
+    final lambda3 = lambda1 + math.atan2(by, math.cos(phi1) + bx);
+
+    // phi3 and lambda3 are actually in radians and LatLng wants degrees
+    return LatLng(latitude: radianToDeg(phi3), longitude: radianToDeg(lambda3));
+  }
 
   @override
   String toString() =>
